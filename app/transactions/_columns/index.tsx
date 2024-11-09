2d69b8cdda973f443bd8b0e4f21f4d3f63b958c7
arthurwillame5 <arthurwillame2005@gmail.com>
2024-11-08 21:09:35 -0300

    feat: refactor transaction type badge rendering into a separate component

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,9 +1,8 @@
-"use client";
+"use client"
 
-import { Badge } from "@/app/_components/ui/badge";
-import { Transaction, TransactionType } from "@prisma/client";
+import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { Circle } from "lucide-react";
+import TransactionTypeBadge from "../_components/type-badge";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -14,25 +13,7 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
     accessorKey: "type",
     header: "Tipo",
     cell: ({ row: { original: transaction } }) => {
-      if (transaction.type === TransactionType.DEPOSIT) {
-        return (
-          <Badge className="bg-muted bg-opacity-10 text-primary hover:bg-muted font-bold">
-            <Circle className="fill-primary size-2 mr-2" /> Depósito
-          </Badge>
-        );
-      }
-      if (transaction.type === TransactionType.EXPENSE) {
-        return (
-          <Badge className="bg-danger bg-opacity-10 text-danger hover:bg-danger/10 font-bold">
-            <Circle className="fill-danger size-2 mr-2" /> Gasto
-          </Badge>
-        );
-      }
-      return (
-        <Badge className="bg-white bg-opacity-10 text-white hover:bg-white/10 font-bold">
-            <Circle className="fill-white size-2 mr-2" /> Investimento
-          </Badge>
-      );
+      <TransactionTypeBadge  transaction={transaction}/>
     },
   },
   {
