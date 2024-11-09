b1d26db63e87d661dac99479f838443e9de9d9c9
arthurwillame5 <arthurwillame2005@gmail.com>
2024-11-09 11:54:37 -0300

    feat: ensure transactions table labels are rendered properly

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,6 +3,9 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
+import { TRANSACTION_CATEGORY_LABELS, TRANSACTION_PAYMENT_METHOD_LABELS } from "@/app/_constants/transactions";
+import { Button } from "@/app/_components/ui/button";
+import { Edit, Pencil, TrashIcon } from "lucide-react";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,17 +22,52 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({row: { original: transaction }}) => {
+      return TRANSACTION_CATEGORY_LABELS[transaction.category];
+    }
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de pagamento",
+    cell: ({ row: { original: transaction } }) => {
+      return TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod];
+    }
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({row: { original: transaction }}) => {
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",  
+        year: "numeric",
+      });
+    }
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) => {
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount));
+    }
   },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: () => {
+      return (
+        <div className="space-x-1">
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <TrashIcon />
+        </Button>
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <Pencil />
+        </Button>
+      </div>
+      )
+    }
+  }
 ];
